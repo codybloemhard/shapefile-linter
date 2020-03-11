95142491e05f05885c0ca997486666e29d0c6434
Cody Bloemhard <cblmhrd@live.nl>
2020-03-11 14:44:27 +0100

    writing binairy file to disk

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate lapp;
 extern crate shapefile;
+extern crate bin_buffer;
 
 use shapefile::*;
+use bin_buffer::*;
+use std::path::Path;
 
 fn main() {
     let args = lapp::parse_args("
@@ -14,7 +17,11 @@ fn main() {
     println!("Shapefile processor...");
     if let Ok(shapes) = shapefile::read(file.clone()){
         println!("Shapes: {}", shapes.len());
-        compressHeightmap(shapes);
+        let shapezs = compress_heightmap(shapes);
+        let mut buffer = Vec::new();
+        shapezs.into_buffer(&mut buffer);
+        let ok = buffer_write_file(&Path::new("heightmap.bin"), &buffer);
+        println!("Writing file went ok?: {}", ok);
     }else{
         println!("Could not read file: {}", file);
     }
@@ -23,34 +30,70 @@ fn main() {
 type P2 = (f64,f64);
 type P3 = (f64,f64,f64);
 
+#[derive(Clone)]
 struct ShapeZ{
     points: Vec<P2>,
     z: f64,
     bb: (P3,P3),
 }
 
-fn compressHeightmap(shapes: Vec<Shape>) -> Vec<ShapeZ>{
+impl Bufferable for ShapeZ{
+    fn into_buffer(self, buf: &mut Buffer){
+        self.z.into_buffer(buf);
+        self.bb.0.into_buffer(buf);
+        self.bb.1.into_buffer(buf);
+        self.points.into_buffer(buf);
+    }
+
+    fn copy_into_buffer(&self, buf: &mut Buffer){
+        self.clone().into_buffer(buf);
+    }
+
+    fn from_buffer(buf: &mut ReadBuffer) -> Option<Self>{
+        let z = if let Some(wz) = f64::from_buffer(buf){ wz }
+        else { return Option::None; };
+        let bb0 = if let Some(wbb0) = <P3>::from_buffer(buf){ wbb0 }
+        else { return Option::None; };
+        let bb1 = if let Some(wbb1) = <P3>::from_buffer(buf){ wbb1 }
+        else { return Option::None; };
+        let len = if let Some(wlen) = u64::from_buffer(buf){ wlen }
+        else { return Option::None; };
+        let mut vec = Vec::new();
+        for _ in 0..len{
+            let p = if let Some(wp) = <P2>::from_buffer(buf){ wp }
+            else { return Option::None; };
+            vec.push(p);
+        }
+        Option::Some(Self{
+            points: vec,
+            z,
+            bb: (bb0,bb1),
+        })
+    }
+}
+
+fn compress_heightmap(shapes: Vec<Shape>) -> Vec<ShapeZ>{
     let mut shapezs = Vec::new();
     'outer: for shape in shapes{
         match shape {
-            Shape::PolylineZ(polylineZ) => {
-                if polylineZ.parts.len() > 1{
+            Shape::PolylineZ(polylinez) => {
+                if polylinez.parts.len() > 1{
                     println!("Warning: skipped shape, more than 1 part!");
                     continue;
                 }
-                if polylineZ.points.is_empty(){
+                if polylinez.points.is_empty(){
                     println!("Warning: skipped shape, 0 points!");
                     continue;
                 }
                 let mut npoints = Vec::new();
-                let z = polylineZ.points[0].z;
+                let z = polylinez.points[0].z;
                 let mut minx = std::f64::MAX;
                 let mut maxx = std::f64::MIN;
                 let mut miny = std::f64::MAX;
                 let mut maxy = std::f64::MIN;
                 let mut minz = std::f64::MAX;
                 let mut maxz = std::f64::MIN;
-                for point in polylineZ.points {
+                for point in polylinez.points {
                     if point.z != z{
                         println!("Warning: skipped shape, not all z equal!");
                         continue 'outer;
