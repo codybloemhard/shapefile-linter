a092cc774e9e0d04bf4cf915fac2bebe6e9194f5
CooleKakkerlak <32516068+CooleKakkerlak@users.noreply.github.com>
2020-04-16 17:23:22 +0200

    triangulation setup

--- src/triangulate.rs
+++ src/triangulate.rs
@@ -1,12 +1,227 @@
 use crate::data::PolygonZ;
+use dlv_list::*;
+use crate::data::*;
+use std::cmp::Ordering;
+use std::borrow::Borrow;
 
 pub struct PolyTriangles{
     vertices: Vec<(f64,f64)>,
     indices: Vec<usize>,
 }
 
+#[derive(Clone,PartialEq)]
+struct PolyPoint{
+    point: P3<f64>,
+    reflex: bool,
+    ear: bool,
+    index: usize
+}
+
 pub fn triangulate(polyzs: Vec<PolygonZ<f64>>) -> Vec<PolyTriangles>
 {
-    let mut res = Vec::new();
-    res
+    println!("hello frog");
+
+    for mut polygon in polyzs{
+        //still need to group inner and outer rings
+
+        let mut vertices = merge_inner(&mut polygon.outers[0], polygon.inners);
+        //vertices.dedup();
+        //if vertices[0] == vertices[vertices.len()-1]{vertices.pop();}
+        let indices = make_indices(&vertices);
+
+        break;
+    }
+
+    return Vec::new();
+}
+
+fn merge_inner(outer: &mut Vec<P3<f64>>, mut inners: Vvec<P3<f64>>) -> Vec<P3<f64>>{
+    //merge inner ring with highest x coordinate first (this one can defneitely to see the outer ring)
+    inners.sort_by(|a, b| rightmost(a).partial_cmp(rightmost(b)).unwrap_or(Ordering::Equal));
+    inners.reverse();
+
+     //merge rings one by one
+    for inner in inners{
+        //get rightmost point in inner ring
+        let mut rightmost = (std::f64::MIN,std::f64::MIN,std::f64::MIN);
+        let mut rightmost_index = 0;
+        for (index,point) in inner.iter().enumerate(){
+            if point.0 > rightmost.0 {
+                rightmost = *point;
+                rightmost_index = index;
+            }
+        }
+
+        //calculate closest intersection with outer ring when going to the right
+        let mut intersect = (0.0,0.0,0.0);
+        let mut intersect_index = 0;
+        let mut best_dis = std::f64::MAX;
+        
+        let x3 = rightmost.0;
+        let y3 = rightmost.1;
+        for i in 0..outer.len(){
+            let x1 = outer[i].0;
+            let y1 = outer[i].1;
+            let x2 = outer[(i + 1) % outer.len()].0;
+            let y2 = outer[(i + 1) % outer.len()].1;
+
+            let t = (y3 - y1) / (y2 - y1);
+            if t < 0.0 || t > 1.0 {continue}
+
+            let x = x1 + t * (x2 - x1);
+            let cur_dis = x - x3;
+            if cur_dis<0.0 || cur_dis >= best_dis {continue}
+
+            best_dis = cur_dis;
+            let z1 = outer[i].2;
+            let z2 = outer[(i + 1) % outer.len()].2;
+            let z = z1 + t * (z2 - z1);
+            intersect_index = (i + 1) % outer.len();
+            intersect = (x,y3,z);
+        }
+        
+        let mut new_vertices: Vec<P3<f64>> = Vec::new();
+        for (i,point) in outer.iter().enumerate(){
+            if i == intersect_index{
+                new_vertices.push(intersect);
+
+                let mut k = rightmost_index;
+                let mut step = 0;
+                while step < inner.len(){
+                    new_vertices.push(inner[k]);
+                    k = (k+1)%inner.len();
+                    step+=1;
+                }
+
+                new_vertices.push(inner[k]);
+                new_vertices.push(intersect);
+            }
+            new_vertices.push(*point);
+        }
+
+        *outer = new_vertices.clone();
+    }
+    return outer.to_vec();
+}
+
+fn rightmost(inner: &Vec<P3<f64>>) -> &f64{
+    let mut rightmost = &std::f64::MIN;
+    for (x,y,z) in inner{
+        if x > rightmost{
+            rightmost = x;
+        }
+    }
+    rightmost
+}
+
+fn make_indices(vertices: &Vec<P3<f64>>) -> Vec<usize>{
+    if vertices.len() < 3 {panic!("polygon can't have fewer than 3 sides")}
+
+    let mut remaining_polygon = VecList::new();
+    let mut orig_indices = Vec::new();
+    for (i,point) in vertices.iter().enumerate(){
+        let p = PolyPoint{
+            point: *point,
+            reflex: false,
+            ear: false,
+            index: i
+        };
+        orig_indices.push(remaining_polygon.push_back(p));
+    }
+
+    let mut i = 0;
+    let clone = &remaining_polygon.clone();
+    for value in remaining_polygon.iter_mut(){
+        value.reflex = is_reflex(clone, orig_indices[i]);
+        i+=1;
+    }
+    i=0;
+    let clone2 = &remaining_polygon.clone();
+    for value in remaining_polygon.iter_mut(){
+        value.ear = is_ear(clone2, orig_indices[i]);
+        i+=1;
+    }
+
+    for (i,point) in remaining_polygon.iter().enumerate(){
+        if(i > 100) {break}
+        println!("index: {}, reflex: {}, ear: {}", i, point.reflex, point.ear);
+    }
+
+    //let mut indices = Vec::new();
+
+
+    return Vec::new();
+}
+
+fn next_cyclic<T>(polygon: &VecList<T>, i: Index<T>) -> &T{
+    if let Some(y) = polygon.get_next_index(i){
+        return polygon.get(y).unwrap();
+    }else{
+        return polygon.front().unwrap();
+    }
+}
+
+fn prev_cyclic<T>(polygon: &VecList<T>, i: Index<T>) -> &T{
+    if let Some(y) = polygon.get_previous_index(i){
+        return polygon.get(y).unwrap();
+    }else{
+        return polygon.back().unwrap();
+    }
+}
+
+fn is_reflex(polygon: &VecList<PolyPoint>, i: Index<PolyPoint>) -> bool{
+    let ax = prev_cyclic(polygon,i).point.0;
+    let ay = prev_cyclic(polygon,i).point.1;
+    let bx = polygon.get(i).unwrap().point.0;
+    let by = polygon.get(i).unwrap().point.1;
+    let cx = next_cyclic(polygon,i).point.0;
+    let cy = next_cyclic(polygon,i).point.1;
+
+    return (bx - ax) * (cy - by) - (cx - bx) * (by - ay) > 0.0;
+}
+
+fn is_ear(polygon: &VecList<PolyPoint>, i: Index<PolyPoint>) -> bool{
+    //an ear is a point of a triangle with no other points inside
+    let p = polygon.get(i).unwrap();
+    let p_prev = prev_cyclic(polygon,i);
+    let p_next = next_cyclic(polygon,i);
+    if p.reflex {return false}
+    for node in polygon{
+        if node == p_prev || node == p || node == p_next {continue}
+        if is_inside_triangle(node.point, p_prev.point, p.point, p_next.point) {
+            return false
+        }
+    }
+    return true
+}
+
+fn is_inside_triangle(p:P3<f64>,p0:P3<f64>,p1:P3<f64>,p2:P3<f64>) -> bool{
+    let x = p.0;
+    let y = p.1;
+    let x1 = p0.0;
+    let y1 = p0.1;
+    let x2 = p1.0;
+    let y2 = p1.1;
+    let x3 = p2.0;
+    let y3 = p2.1;
+    print!("x: {}, ", x);
+    print!("y: {}, ", y);
+    print!("x1: {}, ", x1);
+    print!("y1: {}, ", y1);
+    print!("x2: {}, ", x2);
+    print!("y2: {}, ", y2);
+    print!("x3: {}, ", x3);
+    print!("y3: {}, ", y3);
+
+    panic!("test");
+
+    let denominator = (y2 - y3)*(x1 - x3) + (x3 - x2)*(y1 - y3);
+    let a = ((y2 - y3)*(x - x3) + (x3 - x2)*(y - y3)) / denominator;
+    let b = ((y3 - y1)*(x - x3) + (x1 - x3)*(y - y3)) / denominator;
+    let c = 1.0 - a - b;
+
+    //epsilon is needed because of how inner and outer polygons are merged because
+    //there will be two exactly equal lines in the polygon, only in reversed order
+    let e = 0.0001;
+    return a > 0.0+e && a < 1.0-e && b > 0.0+e && b < 1.0-e && c > 0.0+e && c < 1.0-e;
 }
