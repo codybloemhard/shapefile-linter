34a35042ada4daf10d29bd129db93ca4206aed17
Cody Bloemhard <cblmhrd@live.nl>
2020-04-22 16:36:37 +0200

    change from u64 to T in cut

--- src/chunkify.rs
+++ src/chunkify.rs
@@ -1,5 +1,7 @@
 use crate::data::{ShapeZ, Vvec,BB,MinMax,TTSub,HasBB,CustomShape,BoundingType,UpdateableBB};
 use crate::logger::*;
+use crate::compress::FromU64;
+use std::ops::{Div,Add,Mul};
 
 pub fn bb_in_bb_xy<T>(outer: &BB<T>, inner: &BB<T>) -> bool
     where
@@ -25,31 +27,36 @@ pub type Chunks<T> = Vec<Chunk<T>>;
 
 pub fn cut<T>(cuts: u64, gbb: BB<T>, shapes: &[ShapeZ<T>], logger: &mut Logger) -> Chunks<T>
     where
-        T: Clone + Into<u64> +  BoundingType + MinMax + Copy,
+        T: Clone + Copy + Default + PartialEq + PartialOrd + Into<usize>,
+        T: FromU64 +  BoundingType + MinMax,
+        T: Div<Output = T> + Add<Output = T> + Mul<Output = T>,
 {
-    let mut grid: Vvec<ShapeZ<T>> = vec![vec![]; (cuts * cuts) as usize];
-    let bb0x = (gbb.0).0.into();
-    let bb0y = (gbb.0).1.into();
-    if bb0x != 0 || bb0y != 0{
+    let cuts_u64 = cuts;
+    let cuts = T::from(cuts);
+    let mut grid: Vvec<ShapeZ<T>> = vec![vec![]; (cuts_u64 * cuts_u64) as usize];
+    let bb0x = (gbb.0).0;
+    let bb0y = (gbb.0).1;
+    if bb0x != T::default() || bb0y != T::default(){
         logger.log(Issue::NonOriginBoundingbox);
         return vec![];
     }
-    let bb1x = (gbb.1).0.into();
-    let bb1y = (gbb.1).1.into();
+    let bb1x = (gbb.1).0;
+    let bb1y = (gbb.1).1;
     let gwid = bb1x;
     let ghei = bb1y;
-    let csizex = (gwid / cuts) + 1;
-    let csizey = (ghei / cuts) + 1;
+    let csizex = (gwid / cuts) + T::from(1u64);
+    let csizey = (ghei / cuts) + T::from(1u64);
     for shape in shapes{
         let bb = shape.bounding_box();
-        let x0 = (bb.0).0.into();
-        let y0 = (bb.0).1.into();
-        let x1 = (bb.1).0.into();
-        let y1 = (bb.1).1.into();
+        let x0 = (bb.0).0;
+        let y0 = (bb.0).1;
+        let x1 = (bb.1).0;
+        let y1 = (bb.1).1;
         let cx = x0 / csizex;
         let cy = y0 / csizey;
-        let sbb = ((x0,y0,0),(x1,y1,0));
-        let cbb = ((cx * csizex, cy * csizey, 0),(cx * csizex + csizex, cy * csizey + csizey,0));
+        let sbb = ((x0,y0,T::default()),(x1,y1,T::default()));
+        let cbb = ((cx * csizex, cy * csizey, T::default()),
+                    (cx * csizex + csizex, cy * csizey + csizey,T::default()));
         let inside = bb_in_bb_xy(&cbb, &sbb);
         if !inside {
             let mut old_cx = cx;
@@ -57,18 +64,20 @@ pub fn cut<T>(cuts: u64, gbb: BB<T>, shapes: &[ShapeZ<T>], logger: &mut Logger) 
             let mut points = Vec::new();
             let z = shape.z;
             for (x,y) in shape{
-                let new_cx = (*x).into() / csizex;
-                let new_cy = (*y).into() / csizey;
+                let new_cx = (*x) / csizex;
+                let new_cy = (*y) / csizey;
                 if new_cx == old_cx && new_cy == old_cy{
                     points.push((*x,*y));
                 }else{
+                    let epx = (*x).max_of((cbb.0).0).min_of((cbb.1).0);
+                    let epy = (*y).max_of((cbb.0).1).min_of((cbb.1).1);
                     let mut newshape = ShapeZ{
                         points,
                         z,
                         bb: T::start_box(),
                     };
                     newshape.update_bb();
-                    let vpos = (old_cy * cuts + old_cx) as usize;
+                    let vpos: usize = (old_cy * cuts + old_cx).into();
                     grid[vpos].push(newshape);
                     points = vec![(*x,*y)];
                     old_cx = new_cx;
@@ -81,18 +90,18 @@ pub fn cut<T>(cuts: u64, gbb: BB<T>, shapes: &[ShapeZ<T>], logger: &mut Logger) 
                 bb: T::start_box(),
             };
             newshape.update_bb();
-            let vpos = (old_cy * cuts + old_cx) as usize;
+            let vpos = (old_cy * cuts + old_cx).into();
             grid[vpos].push(newshape);
         }else{
-            let vpos = (cy * cuts + cx) as usize;
+            let vpos = (cy * cuts + cx).into();
             grid[vpos].push(shape.clone());
         }
     }
     let mut chunks = Vec::new();
     for (i,vec) in grid.into_iter().enumerate(){
         let i = i as u64;
-        let x = i % cuts;
-        let y = i / cuts;
+        let x = i % cuts_u64;
+        let y = i / cuts_u64;
         chunks.push((x,y,vec));
     }
     chunks
